879497a7811c3c4e3c6e6f408c373b848c43311f
fer8955 <fernandomeza028@gmail.com>
2025-12-11 23:43:10 -0500

    Commit 18 - Sonar + test

--- src/test/java/com/runner/RunnerTest.java
+++ src/test/java/com/runner/RunnerTest.java
@@ -6,8 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber/cucumber-report.html",
-                "json:target/cucumber/cucumber.json"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber/cucumber-report.html",
+                "json:target/cucumber/cucumber.json"
+        },
         stepNotifications = true,
         features = "src/test/resources/features",
         glue = "com.stepsdefinitions",
