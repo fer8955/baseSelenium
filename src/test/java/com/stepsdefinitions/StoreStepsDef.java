1141942b3fdc5f09f2b91284d0d9da1a441dc69a
fer8955 <fernandomeza028@gmail.com>
2025-12-11 23:16:22 -0500

    Commit 14 - Sonar + test

--- src/test/java/com/stepsdefinitions/StoreStepsDef.java
+++ src/test/java/com/stepsdefinitions/StoreStepsDef.java
@@ -22,9 +22,18 @@ public class StoreStepsDef {
     @Before
     public void setUp(Scenario scenario) {
         this.scenario = scenario;
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
     }
     @After
     public void tearDown() {
